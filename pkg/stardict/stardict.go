06113431c948d149c5bfc1af48089536db198290
Saeed Rasooli <saeed.gnu@gmail.com>
2023-03-20 08:06:49 +0330

    switch to github.com/ilius/go-stardict and support non-exact match lookup sort in order: exact match, prefix, contains

--- pkg/stardict/stardict.go
+++ pkg/stardict/stardict.go
@@ -1,21 +1,16 @@
 package stardict
 
 import (
-	"compress/gzip"
 	"fmt"
-	"io"
+	"html"
 	"os"
 	"path"
-	"path/filepath"
-	"strings"
 
 	"github.com/ilius/ayandict/pkg/common"
-	"github.com/ilius/stardict/pkg/parser"
+	stardict "github.com/ilius/go-stardict"
 )
 
-type DictionaryList []*Dictionary
-
-var dicList DictionaryList
+var dicList []*stardict.Dictionary
 
 func Init() {
 	homeDir, err := os.UserHomeDir()
@@ -23,36 +18,24 @@ func Init() {
 		panic(err)
 	}
 	dicDir := path.Join(homeDir, ".stardict", "dic")
-	dicList, err = Open(dicDir)
+	dicList, err = stardict.Open(dicDir)
 	if err != nil {
 		panic(err)
 	}
 }
-
-func LookupPlaintext(query string) []*common.QueryResult {
-	results := []*common.QueryResult{}
-	for _, dic := range dicList {
-		definitions := dic.GetFormatedMeaning(query)
-		if len(definitions) == 0 {
-			continue
-		}
-		results = append(results, &common.QueryResult{
-			DictName:    dic.GetBookName(),
-			Definitions: definitions,
-		})
-	}
-	return results
-}
-
 func LookupHTML(query string) []*common.QueryResult {
 	results := []*common.QueryResult{}
 	for _, dic := range dicList {
 		definitions := []string{}
-		meaningList := dic.GetWord(query)
-		for _, meaning := range meaningList {
-			for _, defi := range meaning {
-				definitions = append(definitions, string(defi))
+		for _, res := range dic.SearchAuto(query) {
+			defi := fmt.Sprintf(
+				"<b>%s</b>",
+				html.EscapeString(res.Keyword),
+			)
+			for _, item := range res.Items {
+				defi += string(item.Data) + "<br/>"
 			}
+			definitions = append(definitions, defi)
 		}
 		if len(definitions) == 0 {
 			continue
@@ -63,76 +46,4 @@ func LookupHTML(query string) []*common.QueryResult {
 		})
 	}
 	return results
-}
-
-type Dictionary struct {
-	*parser.Dictionary
-}
-
-func (d *Dictionary) Close() {
-}
-
-func LoadDictionary(basePath string) (*Dictionary, error) {
-	ifoFile, err := os.Open(basePath + ".ifo")
-	if err != nil {
-		return nil, fmt.Errorf("failed to open .ifo: %w", err)
-	}
-	defer ifoFile.Close()
-	idxFile, err := os.Open(basePath + ".idx")
-	if err != nil {
-		return nil, fmt.Errorf("failed to open .idx: %w", err)
-	}
-	defer idxFile.Close()
-	var dict io.ReadCloser
-	dict, err = os.Open(basePath + ".dict")
-	if err != nil {
-		if !os.IsNotExist(err) {
-			return nil, fmt.Errorf("failed to open .dict: %w", err)
-		}
-		dictDz, err := os.Open(basePath + ".dict.dz")
-		if err != nil {
-			return nil, fmt.Errorf("failed to open .dict.dz: %w", err)
-		}
-		defer dictDz.Close()
-		dict, err = gzip.NewReader(dictDz)
-		if err != nil {
-			return nil, fmt.Errorf("error in gzip.NewReader: %w", err)
-		}
-	}
-	if dict == nil {
-		return nil, fmt.Errorf("dict == nil")
-	}
-	defer dict.Close()
-
-	pdic, err := parser.LoadDictionary(ifoFile, idxFile, dict)
-	if err != nil {
-		return nil, err
-	}
-	return &Dictionary{
-		Dictionary: pdic,
-	}, nil
-}
-
-func (l DictionaryList) Close() {
-	for _, dic := range l {
-		dic.Close()
-	}
-}
-
-func Open(rootPath string) (DictionaryList, error) {
-	dicList := DictionaryList{}
-	filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
-		if !strings.HasSuffix(path, ".ifo") {
-			return nil
-		}
-		basePath := path[:len(path)-4]
-		dic, err := LoadDictionary(basePath)
-		if err != nil {
-			fmt.Printf("error while opening %v: %v\n", path, err)
-			return nil
-		}
-		dicList = append(dicList, dic)
-		return nil
-	})
-	return dicList, nil
 }
