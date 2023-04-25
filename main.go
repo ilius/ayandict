dfe105657150b8f4971704605bc8b27ddb2473b4
Saeed Rasooli <saeed.gnu@gmail.com>
2023-04-25 08:34:31 +0330

    set error handler for sqldict

--- main.go
+++ main.go
@@ -6,12 +6,16 @@ import (
 
 	"github.com/ilius/ayandict/pkg/application"
 	"github.com/ilius/ayandict/pkg/qerr"
+	sqldict "github.com/ilius/go-dict-sql"
 	"github.com/ilius/go-stardict/v2"
 )
 
 func main() {
 	log.SetOutput(os.Stdout)
 	stardict.ErrorHandler = func(err error) {
+		qerr.Error(err)
+	}
+	sqldict.ErrorHandler = func(err error) {
 		qerr.Error(err)
 	}
 	application.Run()
