3949a6f5cdc79e15a245e116fa7fa8aa8e0b5778
Saeed Rasooli <saeed.gnu@gmail.com>
2023-09-21 11:30:05 +0330

    add -no-gui command line flag

--- main_gui.go
+++ main_gui.go
@@ -3,14 +3,39 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 
 	"github.com/ilius/ayandict/v2/pkg/application"
+	"github.com/ilius/ayandict/v2/pkg/config"
+	"github.com/ilius/ayandict/v2/pkg/dictmgr"
+	"github.com/ilius/ayandict/v2/pkg/server"
 )
 
+func runServerOnly() {
+	conf, err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+	dictmgr.InitDicts(conf, false)
+	server.StartServer(conf.LocalServerPorts[0])
+}
+
 func main() {
+	noGuiFlag := flag.Bool(
+		"no-gui",
+		false,
+		"Do not launch GUI",
+	)
+	flag.Parse()
+
 	log.SetOutput(os.Stdout)
+
+	if *noGuiFlag {
+		runServerOnly()
+		return
+	}
 
 	application.Run()
 }
