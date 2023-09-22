9393abf82d3698d6342565311e8e59ccefc97f86
Saeed Rasooli <saeed.gnu@gmail.com>
2023-09-23 02:32:34 +0330

    switch to GNU Affero General Public License

--- pkg/appinfo/app_info.go
+++ pkg/appinfo/app_info.go
@@ -7,7 +7,7 @@ const VERSION = "v2.0.0"
 const ABOUT = `AyanDict is simple dictionary application based on Qt and written in Go.
 
 Copyleft © 2023 Saeed Rasooli
-AyanDict is licensed by the GNU General Public License version 3 (or later)
+AyanDict is licensed by the GNU Affero General Public License version 3 (or later)
 `
 
 const AUTHORS = `Saeed Rasooli <saeed.gnu@gmail.com> (ilius)`
@@ -15,19 +15,17 @@ const AUTHORS = `Saeed Rasooli <saeed.gnu@gmail.com> (ilius)`
 const LICENSE = `AyanDict - A simple dictionary application
 
 Copyright © 2023 Saeed Rasooli
-This program is free software; you can redistribute it
-and/or modify it under the terms of the GNU General Public
-License as published by the Free Software Foundation; 
-either version 3 of the License, or (at your option) any
-later version.
 
-This program is distributed in the hope that it will be
-useful, but WITHOUT ANY WARRANTY; without even the implied
-warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE. See the GNU General Public License for more details.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-You should have received a copy of the GNU General Public
-License along with this program. Or on Debian systems,
-from /usr/share/common-licenses/GPL.
-If not, see http://www.gnu.org/licenses/gpl.txt
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 `
