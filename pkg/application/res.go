e5f3f398b0b6ff290239dc138c57dfcc82a885b9
Saeed Rasooli <saeed.gnu@gmail.com>
2025-02-19 04:26:35 +0330

    avoid creating temp files for icons / images

--- pkg/application/res.go
+++ pkg/application/res.go
@@ -2,49 +2,22 @@ package application
 
 import (
 	"log/slog"
-	"os"
-	"sync"
 
 	qt "github.com/mappu/miqt/qt6"
 )
 
-var (
-	iconMap      = map[string]*qt.QIcon{}
-	iconMapMutex sync.RWMutex
-)
-
 func loadPNGIcon(filename string) (*qt.QIcon, error) {
-	iconMapMutex.RLock()
-	icon, ok := iconMap[filename]
-	iconMapMutex.RUnlock()
-	if ok {
-		return icon, nil
-	}
 	data, err := res.ReadFile("res/" + filename)
 	if err != nil {
 		return nil, err
 	}
-	file, err := os.CreateTemp("", filename)
-	if err != nil {
-		return nil, err
-	}
-	_, err = file.Write(data)
-	if err != nil {
-		return nil, err
-	}
-	err = file.Close()
-	if err != nil {
-		return nil, err
-	}
-	pixmap := qt.NewQPixmap6(file.Name(), "PNG")
-	icon = qt.NewQIcon2(pixmap)
+	pixmap := qt.NewQPixmap()
+	pixmap.LoadFromData2(data, "PNG")
+	icon := qt.NewQIcon2(pixmap)
 	if icon == nil {
 		slog.Error("error loading png icon: icon is nil: " + filename)
 		panic("error loading png icon: icon is nil")
 	}
-	iconMapMutex.Lock()
-	iconMap[filename] = icon
-	iconMapMutex.Unlock()
 	return icon, nil
 }
 
