a012eff2db783906b7c75d062b67fb684de6ea74
Saeed Rasooli <saeed.gnu@gmail.com>
2025-02-25 22:51:10 +0330

    FavoriteButton: panic and show error dialog if failed to load icon

--- pkg/application/favorite_button.go
+++ pkg/application/favorite_button.go
@@ -1,13 +1,25 @@
 package application
 
-import "github.com/ilius/qt/widgets"
+import (
+	"github.com/ilius/ayandict/v2/pkg/qtcommon/qerr"
+	"github.com/ilius/qt/widgets"
+)
 
 type FavoriteButton struct {
 	*widgets.QPushButton
 }
 
 func NewFavoriteButton() *FavoriteButton {
-	button := NewPNGIconTextButton("", "favorite.png")
+	icon, err := loadPNGIcon("favorite.png")
+	if err != nil {
+		qerr.Error(err)
+		panic(err)
+	}
+	if icon == nil {
+		qerr.Error("error loading favorite.png icon: icon is nil")
+		panic("error loading favorite.png icon: icon is nil")
+	}
+	button := widgets.NewQPushButton3(icon, "", nil)
 	button.SetCheckable(true)
 	return &FavoriteButton{
 		QPushButton: button,
