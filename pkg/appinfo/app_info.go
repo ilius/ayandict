9ba79b8c210bde1a3d76eb18f24ad8224ca11b3b
Saeed Rasooli <saeed.gnu@gmail.com>
2025-10-04 20:33:25 +0330

    version v2.2.4

--- pkg/appinfo/app_info.go
+++ pkg/appinfo/app_info.go
@@ -3,7 +3,7 @@ package appinfo
 const (
 	APP_NAME = "ayandict"
 	APP_DESC = "AyanDict"
-	VERSION  = "v2.2.3"
+	VERSION  = "v2.2.4"
 )
 
 const ABOUT = `A simple cross-platform desktop dictionary application based on Qt framework and written in Go that uses StarDict dictionary format.
