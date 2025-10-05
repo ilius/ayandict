1b15fe1ec5c7f33002ffd9dad0ef928a372ddc31
Saeed Rasooli <saeed.gnu@gmail.com>
2025-10-05 05:08:49 +0330

    showErrorMessage: use standard logger to show panic error

--- pkg/application/error.go
+++ pkg/application/error.go
@@ -1,16 +1,19 @@
 package application
 
 import (
-	"log/slog"
+	"log"
+	"os"
 
 	qt "github.com/mappu/miqt/qt6"
 )
+
+var stdLogger = log.New(os.Stderr, "", log.LstdFlags)
 
 func showErrorMessage(msg string) {
 	defer func() {
 		r := recover()
 		if r != nil {
-			slog.Error("Panic", "r", r)
+			stdLogger.Printf("Panic: %v", r)
 		}
 	}()
 	d := qt.NewQErrorMessage(nil)
