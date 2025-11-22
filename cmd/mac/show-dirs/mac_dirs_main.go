69291a0fc7f31cce9d3ceb600dae6fdbc91cbb56
Saeed Rasooli <saeed.gnu@gmail.com>
2025-11-22 14:05:33 +0330

    use standard API to get directories on mac

--- cmd/mac/show-dirs/mac_dirs_main.go
+++ cmd/mac/show-dirs/mac_dirs_main.go
@@ -1,78 +1,13 @@
 package main
 
-/*
-#cgo CFLAGS: -x objective-c
-#cgo LDFLAGS: -framework Foundation
-#include <Foundation/Foundation.h>
-#include <stdlib.h>
-
-char* getDir(NSSearchPathDirectory dir) {
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(
-        dir,
-        NSUserDomainMask,
-        YES
-    );
-    if ([paths count] == 0) return NULL;
-    NSString *path = [paths objectAtIndex:0];
-    return strdup([path UTF8String]);
-}
-*/
-import "C"
-
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-	"unsafe"
 
-	"github.com/ilius/ayandict/v3/pkg/appinfo"
 	"github.com/ilius/ayandict/v3/pkg/config"
 )
 
-const S_HOME = "HOME"
-
-func foundationDir(dir C.NSSearchPathDirectory) string {
-	cpath := C.getDir(dir)
-	if cpath == nil {
-		return ""
-	}
-	defer C.free(unsafe.Pointer(cpath))
-	return C.GoString(cpath)
-}
-
-func platformConfigDir() string {
-	lib := foundationDir(C.NSLibraryDirectory)
-	if lib == "" {
-		lib = filepath.Join(os.Getenv(S_HOME), "Library")
-	}
-	return filepath.Join(lib, "Preferences", appinfo.APP_DESC)
-}
-
-func GetCacheDir() string {
-	parent := foundationDir(C.NSCachesDirectory)
-	if parent == "" {
-		parent = filepath.Join(os.Getenv(S_HOME), "Library", "Caches")
-	}
-	return filepath.Join(parent, appinfo.APP_DESC)
-}
-
-func GetStateDir() string {
-	parent := foundationDir(C.NSApplicationSupportDirectory)
-	if parent == "" {
-		parent = filepath.Join(
-			os.Getenv(S_HOME),
-			"Library",
-			"Application Support",
-		)
-	}
-	return filepath.Join(parent, appinfo.APP_DESC)
-}
-
 func main() {
-	fmt.Println("Config  (native):", platformConfigDir())
-	fmt.Println("Config (current):", config.GetConfigDir())
-	fmt.Println("Cache  (native):", GetCacheDir())
-	fmt.Println("Cache (current):", config.GetCacheDir())
-	fmt.Println("State  (native):", GetStateDir())
-	fmt.Println("State (current):", config.GetStateDir())
+	fmt.Println("Config:", config.GetConfigDir())
+	fmt.Println("Cache:", config.GetCacheDir())
+	fmt.Println("State:", config.GetStateDir())
 }
--- pkg/config/paths_mac.go
+++ pkg/config/paths_mac.go
@@ -3,36 +3,66 @@
 
 package config
 
+/*
+#cgo CFLAGS: -x objective-c
+#cgo LDFLAGS: -framework Foundation
+#include <Foundation/Foundation.h>
+#include <stdlib.h>
+
+char* getDir(NSSearchPathDirectory dir) {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(
+        dir,
+        NSUserDomainMask,
+        YES
+    );
+    if ([paths count] == 0) return NULL;
+    NSString *path = [paths objectAtIndex:0];
+    return strdup([path UTF8String]);
+}
+*/
+import "C"
+
 import (
 	"os"
 	"path/filepath"
+	"unsafe"
 
 	"github.com/ilius/ayandict/v3/pkg/appinfo"
 )
 
+func foundationDir(dir C.NSSearchPathDirectory) string {
+	cpath := C.getDir(dir)
+	if cpath == nil {
+		return ""
+	}
+	defer C.free(unsafe.Pointer(cpath))
+	return C.GoString(cpath)
+}
+
 func platformConfigDir() string {
-	return filepath.Join(
-		os.Getenv(S_HOME),
-		"Library",
-		"Preferences",
-		appinfo.APP_DESC,
-	)
+	lib := foundationDir(C.NSLibraryDirectory)
+	if lib == "" {
+		lib = filepath.Join(os.Getenv(S_HOME), "Library")
+	}
+	return filepath.Join(lib, "Preferences", appinfo.APP_DESC)
 }
 
 func GetCacheDir() string {
-	return filepath.Join(
-		os.Getenv(S_HOME),
-		"Library",
-		"Caches",
-		appinfo.APP_DESC,
-	)
+	parent := foundationDir(C.NSCachesDirectory)
+	if parent == "" {
+		parent = filepath.Join(os.Getenv(S_HOME), "Library", "Caches")
+	}
+	return filepath.Join(parent, appinfo.APP_DESC)
 }
 
 func GetStateDir() string {
-	return filepath.Join(
-		os.Getenv(S_HOME),
-		"Library",
-		"Application Support",
-		appinfo.APP_DESC,
-	)
+	parent := foundationDir(C.NSApplicationSupportDirectory)
+	if parent == "" {
+		parent = filepath.Join(
+			os.Getenv(S_HOME),
+			"Library",
+			"Application Support",
+		)
+	}
+	return filepath.Join(parent, appinfo.APP_DESC)
 }
