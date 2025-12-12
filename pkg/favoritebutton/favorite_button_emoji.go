10bd865674ecfc2e4e777c419c559f23ffad8d00
Saeed Rasooli <saeed.gnu@gmail.com>
2025-12-12 11:14:28 +0330

    refactor pkg/favoritebutton/favorite_button_emoji.go

--- pkg/favoritebutton/favorite_button_emoji.go
+++ pkg/favoritebutton/favorite_button_emoji.go
@@ -5,7 +5,6 @@ import (
 	"log/slog"
 
 	"github.com/ilius/ayandict/v3/pkg/config"
-	"github.com/ilius/ayandict/v3/pkg/qtutils"
 	qt "github.com/mappu/miqt/qt6"
 )
 
@@ -19,18 +18,13 @@ func NewColoredEmojiFavoriteButton(
 		qButton.SetFixedWidth(event.Size().Height())
 	})
 	qButton.SetText("★") // ★ ⭑ ✯ ☆ ⭐
-	button := &TextFavoriteButton{
-		QPushButton: qButton,
-	}
+	qButton.SetStyleSheet("QPushButton{margin: 0px;}")
 	activeColor, inactiveColor := getEmojiButtonColors(qButton.QWidget, activeHue)
-	button.activeStyleSheet = fmt.Sprintf(
-		"QPushButton{color: %s; margin: 0px;}",
-		activeColor,
-	)
-	button.inactiveStyleSheet = fmt.Sprintf(
-		"QPushButton{color: %s; margin: 0px;}",
-		inactiveColor,
-	)
+	button := &TextFavoriteButton{
+		QPushButton:   qButton,
+		activeColor:   activeColor,
+		inactiveColor: inactiveColor,
+	}
 	button.OnClicked(func() {
 		onClick(button.ToggleChecked())
 	})
@@ -38,7 +32,7 @@ func NewColoredEmojiFavoriteButton(
 }
 
 // returns activeColor, inactiveColor
-func getEmojiButtonColors(button *qt.QWidget, activeHue int) (string, string) {
+func getEmojiButtonColors(button *qt.QWidget, activeHue int) (*qt.QColor, *qt.QColor) {
 	bg := button.Palette().Color(
 		qt.QPalette__Normal,
 		qt.QPalette__Base,
@@ -57,35 +51,41 @@ func getEmojiButtonColors(button *qt.QWidget, activeHue int) (string, string) {
 		"fg", fg.ToQVariant().ToString(),
 	)
 	// "dusk" color scheme: bg.V=127 fg.V=0 bg.L=127 fg.L=0 bg=#7f7f7f fg=#000000
+	active := qt.NewQColor()
+	inactive := qt.NewQColor()
 	if bg.Value() < 130 { // dark BG
-		return qtutils.HSVString(
-				activeHue,
-				110,
-				255-bg.Value()/6,
-			), qtutils.HSVString(
-				0,
-				0,
-				fg.Value()*94/100,
-			)
+		active.SetHsv(
+			activeHue,
+			110,
+			255-bg.Value()/6,
+		)
+		inactive.SetHsv(
+			0,
+			0,
+			fg.Value()*94/100,
+		)
+		return active, inactive
 	}
 	// light BG
-	return qtutils.HSVString(
-			activeHue,
-			100,
-			bg.Value()*3/4,
-		), qtutils.HSVString(
-			0,
-			0,
-			bg.Value()/2,
-		)
+	active.SetHsv(
+		activeHue,
+		100,
+		bg.Value()*3/4,
+	)
+	inactive.SetHsv(
+		0,
+		0,
+		bg.Value()/2,
+	)
+	return active, inactive
 }
 
 type TextFavoriteButton struct {
 	*qt.QPushButton
 	checked bool
 
-	activeStyleSheet   string
-	inactiveStyleSheet string
+	activeColor   *qt.QColor
+	inactiveColor *qt.QColor
 
 	inactiveTooltip string
 	activeTooltip   string
@@ -97,13 +97,24 @@ func (b *TextFavoriteButton) QWidget() *qt.QWidget {
 
 func (b *TextFavoriteButton) SetChecked(checked bool) {
 	b.checked = checked
+	var color *qt.QColor
 	if checked {
 		b.SetToolTip(b.activeTooltip)
-		b.SetStyleSheet(b.activeStyleSheet)
+		color = b.activeColor
 	} else {
 		b.SetToolTip(b.inactiveTooltip)
-		b.SetStyleSheet(b.inactiveStyleSheet)
+		color = b.inactiveColor
 	}
+	b.SetStyleSheet(fmt.Sprintf(
+		"QPushButton{color: %s; margin: 0px;}",
+		color.ToQVariant().ToString(),
+	))
+
+	// this also works (instead of SetStyleSheet), but still probably not reliable
+	// b.Palette().SetColor(qt.QPalette__All, qt.QPalette__ButtonText, color)
+	// b.Palette().SetColor(qt.QPalette__All, qt.QPalette__Text, color)
+	// b.Update()
+	// b.Repaint()
 }
 
 func (b *TextFavoriteButton) ToggleChecked() bool {
